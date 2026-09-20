ba8b2e601019b78538cd076ff74bc71157975e44
keshava <keshavtiwari.dev@gmail.com>
2026-09-20 12:30:16 +0530

    cors updated

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ const requireAuth = require('./middleware/auth')
 
 const app = express()
 
-app.use(cors())
+const allowedOrigins = [
+  'https://mail-sender-fe-chi.vercel.app',
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+]
+
+app.use(cors({
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Origin not allowed by CORS'))
+  },
+}))
 app.use(express.json())
 
 app.get('/api/health', (req, res) => {
